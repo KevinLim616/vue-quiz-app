c5ec393c368704b5bcaac98bf9347b0ccf6da5b1
KevinLim616 <kevinlim198@gmail.com>
2022-12-30 22:10:05 +0800

    using compute instead of watch

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../data/data.json";
 
 const route = useRoute();
@@ -13,12 +13,16 @@ const quiz = quizes.find((quiz) => quiz.id === quizId);
 
 const currentQuestionIndex = ref(0);
 
-const questionStatus = ref(
-  `${currentQuestionIndex.value}/${quiz.questions.length}`
-);
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
 
-watch(currentQuestionIndex, () => {
-  questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+// watch(currentQuestionIndex, () => {
+//   questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+// });
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
 });
 </script>
 <template>
